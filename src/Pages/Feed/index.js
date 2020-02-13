a5933a6790a027ee4bd7cc9f6ded3162e61f29f3
fernandoctech <fernando.ctech@gmail.com>
2020-02-13 19:39:27 -0400

    heroku

--- src/Pages/Feed/index.js
+++ src/Pages/Feed/index.js
@@ -21,16 +21,39 @@ export default class Feed extends Component{
     ){
         return(
             <div className="Feed">
-              <div className="container">
+              <div className="container destak">
                 <div className="destak-title">
-                    <h1><a href="#">NodeJs e Suas <br/> Riquesas</a> </h1><br/>
+                    <h1><a href="#">NodeJs e Suas <br/> Riquezas</a></h1><br/>
                     
                 </div>
                 <div className="destak-img">
                   <img src="https://i.ibb.co/RzxcCGw/Black-Circle-with-Utensils-Restaurant-Logo-1.png" />
                 </div>
               </div>
-               
+              
+              <div className="container-post">
+                <div className="post-feed">
+                {this.state.posts.map(post =>(
+                  <article key={post.id}>
+                  <Link to={`/Post/${post.id}`}>
+                  <div className="card-post">
+                    <img src={post.img} />
+                    <div className="text-overlay">
+                      <h2>{post.title}</h2>
+                     <p>{post.desc}</p>
+                     <strong>author</strong>
+                    </div>
+                    
+                  </div></Link>
+                  
+                  </article>
+                  ))}
+                  
+                  
+                  
+                </div>
+              </div>
+              
             </div>
         );
     }
